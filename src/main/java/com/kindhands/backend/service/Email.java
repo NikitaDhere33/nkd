b3d2b78094fde5133f0f41cfbc1a5c74f9dfd07f
NikitaDhere33 <163223255+NikitaDhere33@users.noreply.github.com>
2026-01-31 16:16:45 +0530

    abc

--- src/main/java/com/kindhands/backend/service/Email.java
+++ src/main/java/com/kindhands/backend/service/Email.java
@@ -1,6 +1,8 @@
 package com.kindhands.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 public class Email {
